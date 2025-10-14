bd43f59c18250c34f6e0906ce7eedc61cbc2b958
Nugusaeyo <chsj0702@gmail.com>
2025-10-14 10:53:19 +0900

    PoolManager

---
+++ Assets/Work/JYG/Code/TestScript.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+namespace Work.JYG.Code
+{
+    public class TestScript : MonoBehaviour
+    {
+        private void Start()
+        {
+            //PoolManager.Instance.PopByName();
+        }
+    }
+}
---
+++ Assets/YGPacks/PoolManager/YgPoolItemSO.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace YGPacks.PoolManager
+{
+    [CreateAssetMenu(fileName = "New YgPoolItem", menuName = "SO/Pool/YgPoolItem", order = 0)]
+    public class YgPoolItemSO : ScriptableObject
+    {
+        [ReadOnly] public string itemName;
+        public string parentName; //Use this if you want the object to have a parent.
+        public GameObject prefab;
+        public int count;
+
+        private void OnValidate()
+        {
+            if (prefab == null) return;
+            
+            IYgPoolable poolable = prefab.GetComponent<IYgPoolable>();
+            if (poolable == null)
+            {
+                Debug.LogError($"Prefab {prefab.name} is not YgPoolable!!");
+                prefab = null;
+                return;
+            }
+            itemName = poolable.Name.Trim();
+            parentName = parentName.Trim();
+        }
+    }
+}
