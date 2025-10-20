6c9f4c4f1a639555616a2f681bbb164dea9a7f5d
ArnoNonim <Radpids2@gmail.com>
2025-10-21 08:35:10 +0900

    feat : BoardTiles

--- Assets/Work/PTY/Scripts/SO/PieceSO.cs
+++ Assets/Work/PTY/Scripts/SO/PieceSO.cs
@@ -1,7 +1,14 @@
 using UnityEngine;
 
+public enum PieceType
+{
+    King, Queen, Bishop, Knight, Rook, Pawn
+}
+
 [CreateAssetMenu(fileName = "Piece", menuName = "SO/Piece")]
 public class PieceSO : ScriptableObject
 {
-    
+    public PieceType type;
+    public Sprite sprite;
+    public Vector2[] interactableTiles;
 }
