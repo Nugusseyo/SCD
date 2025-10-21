855c0865bc1c08e3c2fcb03efb7bd331df9d7b43
ArnoNonim <Radpids2@gmail.com>
2025-10-21 10:59:02 +0900

    feat : PieceInteraction

--- Assets/Work/PTY/Scripts/Piece.cs
+++ Assets/Work/PTY/Scripts/Piece.cs
@@ -4,6 +4,8 @@ public class Piece : MonoBehaviour
 {
     public PieceSO pieceData;
 
+    public bool isSelected;
+
     private SpriteRenderer spriteRenderer;
 
     private void OnValidate()
@@ -13,5 +15,13 @@ public class Piece : MonoBehaviour
         
         if(spriteRenderer != null)
             spriteRenderer.sprite = pieceData.sprite;
+    }
+
+    private void Update()
+    {
+        if (isSelected)
+        {
+            transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0, 0, 10));
+        }
     }
 }
