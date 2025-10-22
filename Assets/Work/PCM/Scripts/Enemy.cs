898a9b81154da953845433cde18b0c98a9131d7a
afw246 <chqwulmin0814@gmail.com>
2025-10-22 23:15:41 +0900

    Enemy move,attack

--- Assets/Work/PCM/Scripts/Enemy.cs
+++ Assets/Work/PCM/Scripts/Enemy.cs
@@ -3,63 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private EnemySO stat;
-    [SerializeField] private LayerMask unit;
-    [SerializeField] Grid grid;
-    private Grid ChessGrid;
+    private EnemyBrain brain;
+    private EnemyAttack attack;
     private int _hp;
     private int _attack;
-    Dictionary<Vector3Int, int> moveValue = new Dictionary<Vector3Int, int>();
-    private Ray ray;
-    private int count = 0;
-
    
     private void Awake()
     {
         _hp = stat.Hp;
         _attack = stat.Attack;  
+        brain = GetComponent<EnemyBrain>();
+        attack = GetComponent<EnemyAttack>();
     }
 
     private void Update()
     {
         if (Keyboard.current.aKey.wasPressedThisFrame)
         {
-            moveValue.Clear();
-            for (int i =0 ; i < stat.EnemyMoveList.Count; i++)
+            if (attack.GetAttack(stat.EnemyAttackList))
             {
-                //Debug.Log(stat.EnemyMoveList[i]); 잘됨 
-                Vector3Int current = grid.WorldToCell(transform.position);
-                
-                Vector3Int exceptionCell =  grid.WorldToCell(current+ stat.EnemyMoveList[i]);
-                Brain(exceptionCell);
-                moveValue.Add(stat.EnemyMoveList[i],count);        
+                Debug.Log("ㅁㄴㅇㅁ");
             }
-            var trans = moveValue.OrderByDescending(x => x.Value).First().Key;
-            transform.position += trans;
-            
+            brain.GetMove(stat.EnemyMoveList, stat.EnemyAttackList);
         }
     }
-    private void Brain(Vector3Int movePos)
-    {
-        count = 0;
-        for (int i = 0; i < stat.EnemyAttackList.Count; i++)
-        {
-            Vector3Int attackOffset = stat.EnemyAttackList[i];
-            Vector3Int centerCell = movePos + attackOffset;
-
-            // 셀 좌표 → 월드 좌표로 변환 (필요 시)
-            Vector3 centerWorld = grid.CellToWorld(centerCell);
-
-            // OverlapBox는 Vector2를 사용하므로 변환
-            Vector2 center = (Vector2)centerWorld;
-            Vector2 boxSize = Vector2.one;
-
-            Collider2D hit = Physics2D.OverlapBox(center, boxSize, 0f, unit);
-            if (hit)
-                count++;
-        }
-    }
+   
 }
