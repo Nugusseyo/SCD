76ed5d0d6b0843913fe76b10e6cf97c14740806e
ArnoNonim <Radpids2@gmail.com>
2025-11-04 12:08:13 +0900

    feat : Attack

--- Assets/Work/PTY/Scripts/ParticlePool.cs
+++ Assets/Work/PTY/Scripts/ParticlePool.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using YGPacks.PoolManager;
 
@@ -5,17 +6,58 @@ namespace Work.PTY.Scripts
 {
     public class ParticlePool : MonoBehaviour, IPoolable
     {
-        public string Name { get; }
-        public GameObject GameObject { get; }
-        
+        public string Name => gameObject.name;
+        public GameObject GameObject => gameObject;
+
+        private ParticleSystem _ps;
+
+        private void Awake()
+        {
+            _ps = GetComponent<ParticleSystem>();
+            gameObject.SetActive(false);
+        }
+
         public void AppearanceItem()
         {
-            throw new System.NotImplementedException();
+            gameObject.SetActive(true);
+            if (_ps != null)
+            {
+                _ps.Play();
+                StartCoroutine(ReturnAfter(_ps));
+            }
+            else
+            {
+
+                StartCoroutine(ReturnAfter(1f));
+            }
+        }
+
+        private IEnumerator ReturnAfter(ParticleSystem ps)
+        {
+            var main = ps.main;
+            // startLifetime은 MinMaxCurve일 수 있으므로 최대값을 계산
+            float lifetime = main.startLifetime.constant;
+            if (main.startLifetime.mode == ParticleSystemCurveMode.TwoConstants)
+                lifetime = Mathf.Max(main.startLifetime.constantMin, main.startLifetime.constantMax);
+
+            float duration = main.duration + lifetime;
+            yield return new WaitForSeconds(duration);
+            PoolManager.Instance.Push(this);
+        }
+
+        private IEnumerator ReturnAfter(float sec)
+        {
+            yield return new WaitForSeconds(sec);
+            PoolManager.Instance.Push(this);
         }
 
         public void ResetItem()
         {
-            throw new System.NotImplementedException();
+            if (_ps != null)
+            {
+                _ps.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+            }
+            gameObject.SetActive(false);
         }
     }
 }
