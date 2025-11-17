49a4b18d8708b957bd9f18eeda9310b881969d54
kimdohyun11 <james49951215@gmail.com>
2025-11-17 15:11:09 +0900

    Feat : Asset

---
+++ Assets/Work/DHN/Scripts/Event/MeteoRenderer.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.Events;
+public class MeteoRenderer : MonoBehaviour
+{
+    public Animator Animator { get; private set; }
+
+    private static readonly int Explosion_Hash = Animator.StringToHash("EXPLOSION");
+
+    public UnityEvent ResetMeteo; 
+
+    private void Awake()
+    {
+        Animator = GetComponent<Animator>();
+    }
+
+    public void ExplosionMeteo()
+    {
+        Animator.SetBool(Explosion_Hash, true);
+    }
+    public void ResetParam()
+    {
+        Animator.SetBool(Explosion_Hash, false);
+    }
+    public void ResetMeteor()
+    {
+        ResetMeteo?.Invoke();
+    }
+}
