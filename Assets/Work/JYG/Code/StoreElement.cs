fbbac4ab8cbbd9934e1e63a9f812d5c74e26d4f3
Nugusaeyo <chsj0702@gmail.com>
2025-11-19 08:37:21 +0900

    UI 배치 수정

--- Assets/Work/JYG/Code/StoreElement.cs
+++ Assets/Work/JYG/Code/StoreElement.cs
@@ -1,6 +1,7 @@
 using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using Work.JYG.Code;
 
 public class StoreElement : MonoBehaviour
@@ -8,14 +9,22 @@ public class StoreElement : MonoBehaviour
     [SerializeField] private int myIndex;
     
     [SerializeField] private TextMeshProUGUI myValue;
+    [SerializeField] private TextMeshProUGUI myPrice;
 
     private void Awake()
     {
         UpdateMyValue();
     }
 
+    public void BuyPiece()
+    {
+        StatManager.Instance.UpgradeMyLevel(myIndex);
+        UpdateMyValue();
+    }
+
     private void UpdateMyValue()
     {
+        myPrice.text = $"{StatManager.Instance.PieceUpgradePrice[myIndex]}";
         myValue.text =
             $"Damage : {StatManager.Instance.PieceDamage[myIndex]} -> {StatManager.Instance.PieceDamage[myIndex] + ((myIndex + 1) * 2)}" +
             $", Hp : {StatManager.Instance.PieceHealth[myIndex]} -> {StatManager.Instance.PieceHealth[myIndex] + ((myIndex + 1) * 20)}";
