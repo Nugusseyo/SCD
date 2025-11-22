1b944cc82a5fcc45972a05098872e9923bb4e6f5
Nugusaeyo <chsj0702@gmail.com>
2025-11-22 19:28:42 +0900

    Coin Manager, ImgSkin

---
+++ Assets/Work/JYG/Code/CoinManager.cs
@@ -0,0 +1,32 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using YGPacks;
+
+namespace Work.JYG.Code
+{
+    public class CoinManager : Singleton<CoinManager>
+    {
+        [SerializeField] private TextMeshProUGUI coinValue;
+        private string c = "C";
+        public int Coin { get; set; }
+
+        protected override void Awake()
+        {
+            base.Awake();
+            Coin = PlayerPrefs.GetInt("Coin", 0);
+            ValueChange();
+        }
+
+        public void ValueChange()
+        {
+            coinValue.text = $"{Coin}C";
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            PlayerPrefs.SetInt("Coin", Coin);
+        }
+    }
+}
