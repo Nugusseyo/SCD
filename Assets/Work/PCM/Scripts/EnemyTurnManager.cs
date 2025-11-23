d36151c04d3f41eb9c5dea31afde6816f1f5f9a0
afw246 <chqwulmin0814@gmail.com>
2025-11-23 12:14:07 +0900

    일단 된듯

--- Assets/Work/PCM/Scripts/EnemyTurnManager.cs
+++ Assets/Work/PCM/Scripts/EnemyTurnManager.cs
@@ -16,18 +16,19 @@ public class EnemyTurnManager : Singleton<EnemyTurnManager>
     [SerializeField] private GameObject[] enemy;
     [SerializeField] private GameObject[] Bossenemy;
     [SerializeField] private Grid grid;
-    [SerializeField] private List<Enemy> Gameobjectlist = new List<Enemy>();
     [SerializeField] private List<Vector3Int> list = new List<Vector3Int>();
-    [SerializeField] private List<Vector3Int> Bosslist = new List<Vector3Int>();
     [SerializeField] private GameObject EnemySprite;
-    [SerializeField] private GameObject BossSprite;
+    [SerializeField] public GameObject BossSprite;
+    public List<GameObject> Bosstlist = new List<GameObject>();
     public ObjectVectorListSO EnemylistSO;
     public int turn; //나중에 도현이가 만든 턴 메니저로 바꾸기
     private int rand;
+
+    public Action BossAction;
     protected override void Awake()
     {
         base.Awake();
-        if(grid == null )
+        if (grid == null)
             grid = FindAnyObjectByType<Grid>();
     }
 
@@ -36,6 +37,22 @@ public class EnemyTurnManager : Singleton<EnemyTurnManager>
         for (int i = 0; i < EnemylistSO.VectorList.Count; i++)
         {
             list.Add(EnemylistSO.VectorList[i]);
+        }
+    }
+    public void Update()
+    {
+        if (list.Count == 0)
+        {
+
+            for (int i = 0; i < EnemylistSO.VectorList.Count; i++)
+            {
+                list.Add(EnemylistSO.VectorList[i]);
+            }
+        }
+        if (Keyboard.current.aKey.wasPressedThisFrame)
+        {
+            Debug.Log("ada");
+            StartCoroutine(EnemyTurn());
         }
     }
     public IEnumerator EnemyTurn()
@@ -54,39 +71,35 @@ public class EnemyTurnManager : Singleton<EnemyTurnManager>
         }
         else
         {
-            EnemySpawn();
+            SpawnCondiction();
         }
         yield return new WaitForSeconds(2f);
         EventManager.Instance.StartCoroutine(EventManager.Instance.EventTrun());
     }
-    public void Update()
+    public void SpawnCondiction()
     {
-        if (list.Count == 0)
+        if (EventManager.Instance.GameTurn >= 20)
         {
-
-            for (int i = 0; i < EnemylistSO.VectorList.Count; i++)
-            {
-                list.Add(EnemylistSO.VectorList[i]);
-            }
+            for (int i = 0; i < Random.Range(EventManager.Instance.GameTurn / 20, (EventManager.Instance.GameTurn / 20) + 1)+1; i++)
+                EnemySpawn();
         }
-        if (Keyboard.current.aKey.wasPressedThisFrame)
+        else
         {
-            StartCoroutine(EnemyTurn());
+            EnemySpawn();
         }
     }
-    
     public void BossEnemySpawn()
     {
         rand = Random.Range(0, list.Count);
         Vector3Int spawn = list[rand];
         list.RemoveAt(rand);
         var enemytrs = grid.GetCellCenterWorld(spawn);
-        GameObject a = Instantiate(Bossenemy[Random.Range(0, Bossenemy.Length)]);
+        GameObject a = Instantiate(Bossenemy[(EventManager.Instance.GameTurn / 20) - 1]);
         a.transform.position = enemytrs;
         Enemy listenemy = a.GetComponent<Enemy>();
         SpriteRenderer em = a.GetComponentInChildren<SpriteRenderer>();
         em.sprite = BossSprite.GetComponent<SpriteRenderer>().sprite;
-        Gameobjectlist.Add(listenemy);
+        Bosstlist.Add(listenemy.gameObject);
         EventManager.Instance.AddList(listenemy);
         listenemy.enabled = false;
 
@@ -105,18 +118,8 @@ public class EnemyTurnManager : Singleton<EnemyTurnManager>
         em.color = Color.black;
 
         listenemy.IsEnd = true;
-        Gameobjectlist.Add(listenemy);
         EventManager.Instance.AddList(listenemy);
         listenemy.enabled = false;
-    }
 
-    public void Jobend()
-    {
-        for (int i = 0; i < Gameobjectlist.Count; i++)
-        {
-            Gameobjectlist[i].IsEnd = false;
-        }
     }
-
-    
 }
